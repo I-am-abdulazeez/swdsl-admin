5b6d43f7ae46091b917151586999802554f0a4ed
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-05-20 05:58:37 +0100

    chore: refactor product component to use the useProduct hook and destructure all returns from the hook.

--- src/components/Product/Products.tsx
+++ src/components/Product/Products.tsx
@@ -1,60 +1,26 @@
 import { Box, HStack, SimpleGrid, Text } from "@chakra-ui/layout";
 import { Spinner } from "@chakra-ui/spinner";
 import { useToast } from "@chakra-ui/toast";
-import {
-  collection,
-  DocumentSnapshot,
-  orderBy,
-  query,
-} from "@firebase/firestore";
-import { useFirestoreQuery } from "@react-query-firebase/firestore";
-import { lazy, Suspense, useEffect } from "react";
+import { DocumentSnapshot } from "@firebase/firestore";
+import { lazy, Suspense } from "react";
 import { Link } from "react-router-dom";
 
-import { firebaseFirstore } from "@lib/firebase/index";
+import { useProduct } from "@hooks/useProduct";
 
 const ProductList = lazy(() => import("./ProductList"));
 
-const Products = (): JSX.Element => {
-  const toast = useToast();
-  const ref = query(
-    collection(firebaseFirstore, "products"),
-    orderBy("createdAt", "desc")
-  );
-  const storeQuery = useFirestoreQuery(
-    ["products"],
-    ref,
-    {},
-    {
-      onError(err) {
-        // console.log(err);
-        toast({
-          title: `Error fetching data ${err.message}`,
-          status: "error",
-          variant: "subtle",
-          duration: 5000,
-          isClosable: true,
-        });
-      },
-    }
-  );
-  const snapshot = storeQuery.data;
-
-  // console.log(snapshot);
-
-  useEffect(() => {
-    console.log(snapshot);
-  }, [snapshot]);
+const Products: React.FC = () => {
+  const { products, storeQuery, emptyProduct } = useProduct();
 
   return (
     <Box mt={8}>
-      {snapshot?.empty && (
+      {emptyProduct && (
         <Text>No product added. click on Drinks upload to add one</Text>
       )}
       {storeQuery.isLoading && (
         <HStack spacing={2}>
           <Spinner
-            thickness="2px"
+            thickness="1px"
             speed="0.65s"
             emptyColor="gray.200"
             size="sm"
@@ -65,7 +31,7 @@ const Products = (): JSX.Element => {
       )}
       {storeQuery.isLoadingError && <Text>Error Fetching product</Text>}
       <SimpleGrid columns={{ base: 1, md: 2 }} spacing={3}>
-        {snapshot?.docs?.map((docsSnapshot: DocumentSnapshot) => {
+        {products.map((docsSnapshot: DocumentSnapshot) => {
           const product = docsSnapshot.data();
           return (
             <Link to={`/product/${docsSnapshot.id}`} key={docsSnapshot.id}>
