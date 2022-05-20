9adf05d97e683a9ac48e74a310a8fac57cfc64ce
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-05-20 05:56:44 +0100

    feat: useProduct hook to display products

---
+++ src/hooks/useProduct/index.ts
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { useFirestoreQuery } from "@react-query-firebase/firestore";
+import {
+  collection,
+  DocumentData,
+  orderBy,
+  query,
+  QueryDocumentSnapshot,
+} from "firebase/firestore";
+
+import { firebaseFirstore } from "@lib/firebase";
+import { useToast } from "@chakra-ui/react";
+
+export const useProduct = () => {
+  const [products, setProducts] = useState<
+    QueryDocumentSnapshot<DocumentData>[]
+  >([]);
+  const ref = query(
+    collection(firebaseFirstore, "products"),
+    orderBy("createdAt", "desc")
+  );
+
+  const chakraToast = useToast();
+
+  const storeQuery = useFirestoreQuery(
+    ["products"],
+    ref,
+    { subscribe: true, includeMetadataChanges: true },
+    {
+      onSuccess: (data) => {
+        const products = data?.docs;
+        setProducts(products);
+      },
+      onError: (error) => {
+        chakraToast({
+          status: "error",
+          title: `${error.message}`,
+          isClosable: true,
+          containerStyle: {
+            fontSize: "12.5px",
+          },
+          variant: "subtle",
+          position: "bottom-right",
+        });
+      },
+    }
+  );
+
+  return {
+    products,
+    storeQuery,
+  };
+};
