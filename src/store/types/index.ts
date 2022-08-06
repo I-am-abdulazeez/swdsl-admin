b4257b974f7310b5cc7f551815b923b70a72d291
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-08-06 20:36:01 +0100

    chore: created types for AuthStore

---
+++ src/store/types/index.ts
@@ -0,0 +1,15 @@
+import { Admin } from '@interfaces/index';
+import { User } from 'firebase/auth';
+
+export type AuthState = {
+  user: User | null;
+  isLoading: boolean | undefined;
+  isLoggedIn: boolean;
+};
+
+export type AuthActions = {
+  signInAdmin: (user: Admin) => void;
+  signOutAdmin: () => void;
+};
+
+export type AuthStore = AuthState & AuthActions;
