a8076c5ff699b7af50cce7a6bd6d27bc2ca1db32
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-08-06 20:35:04 +0100

    chore: component composition

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,77 @@
-import { lazy, Suspense } from "react";
-import { Box, Container, Flex, Spinner } from "@chakra-ui/react";
-import { Routes, Route } from "react-router-dom";
+import { lazy, Suspense } from 'react';
+import { Box, Container, Flex, Spinner } from '@chakra-ui/react';
+import { Routes, Route } from 'react-router-dom';
 
-import { AnonymousRoute, PrivateRoute } from "@components/Routes";
+import { AnonymousRoute, PrivateRoute } from '@components/Routes';
 
-const Login = lazy(() => import("@pages/login"));
-const Home = lazy(() => import("@pages/index"));
-const Dashboard = lazy(() => import("@pages/dashboard"));
-const ProductDetails = lazy(() => import("@components/Product/ProductDetails"));
-const Upload = lazy(() => import("@pages/upload"));
-const Orders = lazy(() => import("@pages/orders"));
+const Login = lazy(() => import('@pages/login'));
+const Home = lazy(() => import('@pages/index'));
+const Dashboard = lazy(() => import('@pages/dashboard'));
+const ProductDetails = lazy(() => import('@components/Product/ProductDetails'));
+const Upload = lazy(() => import('@pages/upload'));
+const Orders = lazy(() => import('@pages/orders'));
 
 const App: React.FC = () => {
   return (
-    <Box as="main" py={10} display="flex" alignItems="center">
-      <Container maxW="container.lg">
-        <Suspense
-          fallback={
-            <Flex height={"90vh"} justifyContent="center" alignItems={"center"}>
-              <Spinner colorScheme={"primary.400"} />
-            </Flex>
-          }
-        >
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route
-              path="/dashboard"
-              element={
-                <PrivateRoute>
-                  <Dashboard />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/product/:id"
-              element={
-                <PrivateRoute>
-                  <ProductDetails />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/login"
-              element={
-                <AnonymousRoute>
-                  <Login />
-                </AnonymousRoute>
-              }
-            />
-            <Route
-              path="/orders"
-              element={
-                <PrivateRoute>
-                  <Orders />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/upload"
-              element={
-                <PrivateRoute>
-                  <Upload />
-                </PrivateRoute>
-              }
-            />
-          </Routes>
-        </Suspense>
-      </Container>
-    </Box>
+    <Flex
+      as="main"
+      py={10}
+      align={'center'}
+      height={'80vh'}
+      overflowY={'scroll'}
+    >
+      <Suspense
+        fallback={
+          <Flex height={'90vh'} justifyContent="center" alignItems={'center'}>
+            <Spinner colorScheme={'primary.400'} />
+          </Flex>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/dashboard"
+            element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/product/:id"
+            element={
+              <PrivateRoute>
+                <ProductDetails />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <AnonymousRoute>
+                <Login />
+              </AnonymousRoute>
+            }
+          />
+          <Route
+            path="/orders"
+            element={
+              <PrivateRoute>
+                <Orders />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/upload"
+            element={
+              <PrivateRoute>
+                <Upload />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </Suspense>
+    </Flex>
   );
 };
 
