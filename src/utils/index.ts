f185a80fe5375e16e717f6eb21bee8d5141232d7
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-08-07 18:23:14 +0100

    chore: created a resuable function for a toast

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,19 @@
+import { createStandaloneToast, UseToastOptions } from '@chakra-ui/react';
+
+import customTheme from '@assets/theme';
+
 export const numberWithCommas = (x: string) => {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+};
+
+export const customToast = (options?: UseToastOptions | undefined) => {
+  const { toast } = createStandaloneToast({ theme: customTheme });
+  return toast({
+    duration: 4000,
+    isClosable: true,
+    containerStyle: {
+      fontSize: '12px',
+    },
+    ...options,
+  });
 };
