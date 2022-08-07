86a4086c6246c1851745aac33b0c68941d9fc39f
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-08-07 19:05:43 +0100

    chore: remove ClienOnly component and passing history to Router component

--- src/components/AppContainer/AppContainer.tsx
+++ src/components/AppContainer/AppContainer.tsx
@@ -1,23 +1,12 @@
-import { Flex, Spinner } from '@chakra-ui/react';
+import { Flex } from '@chakra-ui/react';
 import { PropsWithChildren, Suspense } from 'react';
+
+import FallBackComponent from '@components/FallBackComponent';
 
 const AppContainer: React.FC<PropsWithChildren> = ({ children }) => {
   return (
-    <Flex as="main" py={10} alignItems={'center'} height={'80vh'}>
-      <Suspense
-        fallback={
-          <Flex
-            height={'90vh'}
-            width={'100%'}
-            justifyContent="center"
-            alignItems={'center'}
-          >
-            <Spinner colorScheme={'primary.400'} />
-          </Flex>
-        }
-      >
-        {children}
-      </Suspense>
+    <Flex as="main" py={10} alignItems={'center'}>
+      <Suspense fallback={<FallBackComponent />}>{children}</Suspense>
     </Flex>
   );
 };
