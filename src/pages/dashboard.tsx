6bc60e79df3e7a6dc2fbadb5c54b68a6b5060dc8
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-08-07 23:34:06 +0100

    chore: fetchingProduct onUseLayoutEffect

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,16 +1,23 @@
-import { lazy, Suspense } from "react";
-import { Box, Text } from "@chakra-ui/layout";
-import { Flex } from "@chakra-ui/react";
+import { lazy, Suspense, useLayoutEffect } from 'react';
+import { Box, Text } from '@chakra-ui/layout';
+import { Flex } from '@chakra-ui/react';
 
-import DrinkSearch from "@components/DrinkSearch";
+import DrinkSearch from '@components/DrinkSearch';
+import { useProductStore } from '@store/useProductStore';
 
-const Products = lazy(() => import("@components/Product/Products"));
+const Products = lazy(() => import('@components/Product/Products'));
 
 const Dashboard: React.FC = () => {
+  const fetchProducts = useProductStore((state) => state.fetchProducts);
+
+  useLayoutEffect(() => {
+    return () => fetchProducts();
+  });
+
   return (
     <Box>
       <Flex
-        flexDir={{ base: "column", md: "row" }}
+        flexDir={{ base: 'column', md: 'row' }}
         justifyContent="space-between"
       >
         <Text mb={{ base: 4, md: 0 }} fontSize="xl" fontWeight="semibold">
