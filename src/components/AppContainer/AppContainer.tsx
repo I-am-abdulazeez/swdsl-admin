b8c2d04a171d6b46555b7d3414950e57882f50a2
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-08-07 19:19:20 +0100

    chore: added container component to centralize app

--- src/components/AppContainer/AppContainer.tsx
+++ src/components/AppContainer/AppContainer.tsx
@@ -1,4 +1,4 @@
-import { Flex } from '@chakra-ui/react';
+import { Container, Flex } from '@chakra-ui/react';
 import { PropsWithChildren, Suspense } from 'react';
 
 import FallBackComponent from '@components/FallBackComponent';
@@ -6,7 +6,9 @@ import FallBackComponent from '@components/FallBackComponent';
 const AppContainer: React.FC<PropsWithChildren> = ({ children }) => {
   return (
     <Flex as="main" py={10} alignItems={'center'}>
-      <Suspense fallback={<FallBackComponent />}>{children}</Suspense>
+      <Suspense fallback={<FallBackComponent />}>
+        <Container maxWidth={'container.lg'}>{children}</Container>
+      </Suspense>
     </Flex>
   );
 };
