1e106601b5f5c37f42890cfa5fb8204a87426022
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-08-07 19:06:12 +0100

    chore: created FallbackComponent: this loads spinner

---
+++ src/components/FallBackComponent/index.tsx
@@ -0,0 +1,16 @@
+import { Flex, Spinner } from '@chakra-ui/react';
+
+const FallBackComponent = () => {
+  return (
+    <Flex
+      height={'90vh'}
+      width={'100%'}
+      justifyContent="center"
+      alignItems={'center'}
+    >
+      <Spinner colorScheme={'primary'} />
+    </Flex>
+  );
+};
+
+export default FallBackComponent;
