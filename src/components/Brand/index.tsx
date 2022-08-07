3000a57161d8c569badd12613a989c974a80cc93
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-08-07 18:22:07 +0100

    style: logo size change

--- src/components/Brand/index.tsx
+++ src/components/Brand/index.tsx
@@ -1,18 +1,14 @@
-import { Image } from "@chakra-ui/image";
-import { Box } from "@chakra-ui/layout";
-import { Link } from "react-router-dom";
+import { Image } from '@chakra-ui/image';
+import { Box } from '@chakra-ui/layout';
+import { Link } from 'react-router-dom';
 
-import ShayoLogo from "@assets/images/swdsl-logo.svg";
+import ShayoLogo from '@assets/images/swdsl-logo.svg';
 
 const Brand: React.FC = () => {
   return (
     <Box>
       <Link to="/">
-        <Image
-          src={ShayoLogo}
-          width={{ base: "100px", md: "125px" }}
-          alt="shayo-logo"
-        />
+        <Image src={ShayoLogo} width={'100px'} alt="shayo-logo" />
       </Link>
     </Box>
   );
