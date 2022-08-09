f018657a0bfd0d011519092d106ab727b8557808
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-08-09 11:42:09 +0100

    chore: Added categoryType and UploadImage function params.

---
+++ src/types/index.ts
@@ -0,0 +1,28 @@
+import { Timestamp } from 'firebase/firestore';
+
+export type CategoryType =
+  | 'Cognac'
+  | 'Juice'
+  | 'Whisky'
+  | 'Non alcoholic'
+  | 'Red wine'
+  | 'Champagne'
+  | 'Rum'
+  | 'Irish cream'
+  | 'White wine'
+  | 'Gin'
+  | 'Sparkling wine'
+  | 'Brandy'
+  | 'Juice'
+  | 'Tequila'
+  | 'Vodka'
+  | 'Others';
+
+export type UploadImageParams = {
+  file: File | null;
+  progress: number;
+  setUploadLoading: React.Dispatch<React.SetStateAction<boolean>>;
+  setProgress: React.Dispatch<React.SetStateAction<number>>;
+  setUrl: React.Dispatch<React.SetStateAction<string>>;
+  setCreatedAt: React.Dispatch<React.SetStateAction<Timestamp | null>>;
+};
