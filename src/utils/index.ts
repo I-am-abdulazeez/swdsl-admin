8efe09f39e0e08f748e6a4ae6ecca8ebb7a21e50
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-08-09 11:39:26 +0100

    chore: added uploadImage function with some params since it's a resuable function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,6 +2,9 @@ import { createStandaloneToast, UseToastOptions } from '@chakra-ui/react';
 
 import customTheme from '@assets/theme';
 import { createBrowserHistory } from 'history';
+import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
+import { firebaseStorage, timestamp } from '@lib/firebase';
+import { UploadImageParams } from 'src/types';
 
 export const numberWithCommas = (x: string) => {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
@@ -20,3 +23,57 @@ export const customToast = (options?: UseToastOptions | undefined) => {
 };
 
 export const Router = createBrowserHistory({ window });
+
+export const uploadImage = ({
+  file,
+  progress,
+  setCreatedAt,
+  setProgress,
+  setUploadLoading,
+  setUrl,
+}: UploadImageParams) => {
+  const storageRef = ref(firebaseStorage, `upload/${file?.name}`);
+  const uploadTask = uploadBytesResumable(storageRef, file!);
+
+  uploadTask.on(
+    'state_changed',
+    (snapshot) => {
+      setUploadLoading(true);
+      let percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      setProgress(percentage);
+      console.log('Upload is ' + progress + '% done');
+      switch (snapshot.state) {
+        case 'paused':
+          console.log('Upload is paused');
+          break;
+        case 'running':
+          console.log('Upload is running');
+          break;
+      }
+      console.log('Upload is ' + progress + '% done');
+    },
+    (error) => {
+      console.log(error);
+      setUploadLoading(false);
+      customToast({
+        status: 'error',
+        title: `${error.message}`,
+      });
+    },
+    async () => {
+      // On Success
+      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+        console.log('File available at ', downloadURL);
+        const url = downloadURL;
+        const createdAt = timestamp;
+        setUrl(url);
+        setCreatedAt(createdAt);
+        customToast({
+          status: 'success',
+          title: `Product image uploaded to storage successfully`,
+        });
+        setUploadLoading(false);
+      });
+    }
+  );
+};
