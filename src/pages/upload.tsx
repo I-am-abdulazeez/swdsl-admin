b851a15f559995287867f8b120d223922c624a82
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-08-10 02:00:13 +0100

    chore:utilize FormDetails component in upload page

--- src/pages/upload.tsx
+++ src/pages/upload.tsx
@@ -1,24 +1,12 @@
 import { useState } from 'react';
-import {
-  Box,
-  Text,
-  SimpleGrid,
-  ButtonGroup,
-  Button,
-  Input,
-  FormControl,
-  FormLabel,
-  Select,
-} from '@chakra-ui/react';
+import { Text, ButtonGroup, Button } from '@chakra-ui/react';
 import { Timestamp } from '@firebase/firestore';
 import { SubmitHandler, useForm } from 'react-hook-form';
 
-import { FileInput } from '@components/FormFields';
-
-import { drinkCategoriesArray } from '@data/index';
 import { UploadFormState } from '@interfaces/index';
 import { uploadImage } from '@utils/index';
 import { useProductStore } from '@store/useProductStore';
+import FormDetails from '@components/FormDetails';
 
 const Upload: React.FC = () => {
   const { register, handleSubmit, reset } = useForm<UploadFormState>();
@@ -62,66 +50,13 @@ const Upload: React.FC = () => {
         Upload Drinks
       </Text>
       <form onSubmit={handleSubmit(handleProductSubmit)}>
-        <SimpleGrid columns={{ base: 1, sm: 2 }} spacing={6} textAlign="left">
-          <FormControl>
-            <FormLabel fontSize="14px">Drink name</FormLabel>
-            <Input
-              {...register('drinkName', { required: true })}
-              type={'text'}
-              placeholder="Meridian"
-            />
-          </FormControl>
-          <FormControl>
-            <FormLabel fontSize="14px">Description</FormLabel>
-            <Input
-              {...register('description', { required: true })}
-              type={'text'}
-              placeholder="Meridian wine is the best"
-            />
-          </FormControl>
-          <FormControl>
-            <FormLabel fontSize="14px">Select Category</FormLabel>
-            <Select
-              {...register('category', { required: true })}
-              placeholder="Select category"
-            >
-              {drinkCategoriesArray.map(({ drinkCategory, drink_id }) => (
-                <option key={drink_id} value={drinkCategory}>
-                  {drinkCategory}
-                </option>
-              ))}
-            </Select>
-          </FormControl>
-          <FormControl>
-            <FormLabel fontSize="14px">Price</FormLabel>
-            <Input
-              {...register('price', { required: true })}
-              type="number"
-              placeholder="500 (in Dollars)"
-            />
-          </FormControl>
-        </SimpleGrid>
-
-        <Box my={3}>
-          <Text fontSize="sm" fontWeight="normal">
-            Click on the circle below to select a drink image
-          </Text>
-        </Box>
-        <Box
-          display="flex"
-          p={3}
-          borderRadius="10px"
-          flexDir="column"
-          border="1px solid #EDF2F7"
-        >
-          <FileInput
-            file={file}
-            progress={progress}
-            setProgress={setProgress}
-            setFile={setFile}
-          />
-        </Box>
-
+        <FormDetails
+          register={register}
+          file={file}
+          progress={progress}
+          setFile={setFile}
+          setProgress={setProgress}
+        />
         <ButtonGroup mt={4} spacing={2}>
           <Button
             size="sm"
