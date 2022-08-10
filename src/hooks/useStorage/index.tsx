1b7beb3484fe14ed211d709c75f0364d62ce4392
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-08-10 11:41:36 +0100

    Merge branch 'main' of https://github.com/I-am-abdulazeez/swdsl-admin

--- src/hooks/useStorage/index.tsx
+++ src/hooks/useStorage/index.tsx
@@ -1,41 +1,41 @@
-import { getDownloadURL, ref, uploadBytesResumable } from "@firebase/storage";
-import { useState, useEffect } from "react";
-import { Timestamp } from "firebase/firestore";
-import { useToast } from "@chakra-ui/toast";
+import { getDownloadURL, ref, uploadBytesResumable } from '@firebase/storage';
+import { useState, useEffect } from 'react';
+import { Timestamp } from 'firebase/firestore';
+import { useToast } from '@chakra-ui/toast';
 
-import { firebaseStorage, timestamp } from "@lib/firebase";
+import { firebaseStorage, timestamp } from '@lib/firebase';
 
 export const useStorage = (file: File | null) => {
   const toast = useToast();
   const [progress, setProgress] = useState<number>(0);
-  const [error, setError] = useState("");
-  const [url, setUrl] = useState("");
-  const [createdAt, setCreatedAt] = useState<Timestamp | string>("");
+  const [error, setError] = useState('');
+  const [url, setUrl] = useState('');
+  const [createdAt, setCreatedAt] = useState<Timestamp | string>('');
 
   useEffect(() => {
     const storageRef = ref(firebaseStorage, `upload/${file?.name}`);
     const uploadTask = uploadBytesResumable(storageRef, file!);
 
     uploadTask.on(
-      "state_changed",
+      'state_changed',
       (snapshot) => {
         let percentage =
           (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
         setProgress(percentage);
-        console.log("Upload is " + progress + "% done");
+        console.log('Upload is ' + progress + '% done');
         switch (snapshot.state) {
-          case "paused":
-            console.log("Upload is paused");
+          case 'paused':
+            console.log('Upload is paused');
             break;
-          case "running":
-            console.log("Upload is running");
+          case 'running':
+            console.log('Upload is running');
             break;
         }
       },
       (error) => {
         setError(error.message);
         toast({
-          status: "error",
+          status: 'error',
           title: `${error.message}`,
           isClosable: true,
           duration: 3000,
@@ -44,13 +44,13 @@ export const useStorage = (file: File | null) => {
       async () => {
         // On Success
         getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-          console.log("File available at ", downloadURL);
+          console.log('File available at ', downloadURL);
           const url = downloadURL;
           const createdAt = timestamp;
           setUrl(url);
           setCreatedAt(createdAt);
           toast({
-            status: "success",
+            status: 'success',
             title: `File upploaded to storage successfully`,
             isClosable: true,
             duration: 3000,
