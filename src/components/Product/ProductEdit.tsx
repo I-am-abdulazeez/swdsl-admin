569cfb843b8d3d0c0e33493599b3603a5694f2c2
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-08-10 10:08:35 +0100

    chore: remove userId feom store, renamed ref from stuff to 'ref'

--- src/components/Product/ProductEdit.tsx
+++ src/components/Product/ProductEdit.tsx
@@ -26,111 +26,105 @@ import { uploadImage } from '@utils/index';
 import { UploadFormState } from '@interfaces/index';
 import { ProductEditParams, ProductType } from 'src/types';
 
-const ProductEdit = forwardRef(
-  ({ product }: ProductType, StuffRef): JSX.Element => {
-    const { userId } = useAuthStore();
-    const { id } = useParams<ProductEditParams>();
-    const { register, handleSubmit } = useForm<UploadFormState>();
+const ProductEdit = forwardRef(({ product }: ProductType, ref): JSX.Element => {
+  const { id } = useParams<ProductEditParams>();
+  const { register, handleSubmit } = useForm<UploadFormState>();
 
-    const [file, setFile] = useState<File | null>(null);
-    const [uploadLoading, setUploadLoading] = useState<boolean>(false);
-    const [url, setUrl] = useState<string>('');
-    const [progress, setProgress] = useState<number>(0);
-    const { isOpen, onOpen, onClose } = useDisclosure();
+  const [file, setFile] = useState<File | null>(null);
+  const [uploadLoading, setUploadLoading] = useState<boolean>(false);
+  const [url, setUrl] = useState<string>('');
+  const [progress, setProgress] = useState<number>(0);
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-    const isLoading = useProductStore((state) => state.isLoadingSave);
+  const isLoading = useProductStore((state) => state.isLoadingSave);
 
-    const handleProductImageUpload = () => {
-      uploadImage({
-        file,
-        progress,
-        setProgress,
-        setUploadLoading,
-        setUrl,
-      });
+  const handleProductImageUpload = () => {
+    uploadImage({
+      file,
+      progress,
+      setProgress,
+      setUploadLoading,
+      setUrl,
+    });
+  };
+
+  const handleProductEdit: SubmitHandler<UploadFormState> = (data) => {
+    const { drinkName, description, category, price } = data;
+    const newUpdate = {
+      drinkName: drinkName || product?.drinkName,
+      description: description || product?.description,
+      category: category || product?.category,
+      url: url || product?.url,
+      price: price || product?.price,
     };
+    console.log(newUpdate);
+    // docsEditMutation.mutate(newUpdate);
+  };
 
-    const handleProductEdit: SubmitHandler<UploadFormState> = (data) => {
-      const { drinkName, description, category, price } = data;
-      const newUpdate = {
-        drinkName: drinkName || product?.drinkName,
-        description: description || product?.description,
-        category: category || product?.category,
-        url: url || product?.url,
-        price: price || product?.price,
-      };
-      console.log(newUpdate);
-      // docsEditMutation.mutate(newUpdate);
-    };
-
-    return (
-      <Box>
-        <IconButton
-          aria-label="edit-button"
-          size="sm"
-          colorScheme="secondary"
-          variant="ghost"
-          onClick={onOpen}
-          icon={<RiPencilLine size="18px" />}
-        />
-        <Modal
-          isCentered
-          size="lg"
-          onClose={onClose}
-          isOpen={isOpen}
-          motionPreset="slideInBottom"
-          closeOnOverlayClick={false}
-        >
-          <ModalOverlay backdropFilter={'blur(4px)'} />
-          <ModalContent>
-            <ModalHeader>
-              Edit Product{' '}
-              <chakra.span color="success.700">
-                {product?.drinkName}
-              </chakra.span>
-              ?
-            </ModalHeader>
-            <ModalCloseButton size="sm" />
-            <form onSubmit={handleSubmit(handleProductEdit)}>
-              <ModalBody>
-                <FormDetails
-                  register={register}
-                  file={file}
-                  progress={progress}
-                  setFile={setFile}
-                  setProgress={setProgress}
-                  showValue={true}
-                  isRequired={false}
-                  product={product}
-                />
-              </ModalBody>
-              <ModalFooter>
-                <ButtonGroup mt={4} spacing={2}>
-                  <Button
-                    isLoading={uploadLoading}
-                    onClick={handleProductImageUpload}
-                    isDisabled={!file || progress === 100}
-                    size="sm"
-                  >
-                    Upload Image First
-                  </Button>
-                  <Button
-                    size="sm"
-                    type="submit"
-                    variant="solid"
-                    isLoading={isLoading}
-                    colorScheme="success"
-                  >
-                    Update All
-                  </Button>
-                </ButtonGroup>
-              </ModalFooter>
-            </form>
-          </ModalContent>
-        </Modal>
-      </Box>
-    );
-  }
-);
+  return (
+    <Box>
+      <IconButton
+        aria-label="edit-button"
+        size="sm"
+        colorScheme="secondary"
+        variant="ghost"
+        onClick={onOpen}
+        icon={<RiPencilLine size="18px" />}
+      />
+      <Modal
+        isCentered
+        size="lg"
+        onClose={onClose}
+        isOpen={isOpen}
+        motionPreset="slideInBottom"
+        closeOnOverlayClick={false}
+      >
+        <ModalOverlay backdropFilter={'blur(4px)'} />
+        <ModalContent>
+          <ModalHeader>
+            Edit Product{' '}
+            <chakra.span color="success.700">{product?.drinkName}</chakra.span>?
+          </ModalHeader>
+          <ModalCloseButton size="sm" />
+          <form onSubmit={handleSubmit(handleProductEdit)}>
+            <ModalBody>
+              <FormDetails
+                register={register}
+                file={file}
+                progress={progress}
+                setFile={setFile}
+                setProgress={setProgress}
+                showValue={true}
+                isRequired={false}
+                product={product}
+              />
+            </ModalBody>
+            <ModalFooter>
+              <ButtonGroup mt={4} spacing={2}>
+                <Button
+                  isLoading={uploadLoading}
+                  onClick={handleProductImageUpload}
+                  isDisabled={!file || progress === 100}
+                  size="sm"
+                >
+                  Upload Image First
+                </Button>
+                <Button
+                  size="sm"
+                  type="submit"
+                  variant="solid"
+                  isLoading={isLoading}
+                  colorScheme="success"
+                >
+                  Update All
+                </Button>
+              </ButtonGroup>
+            </ModalFooter>
+          </form>
+        </ModalContent>
+      </Modal>
+    </Box>
+  );
+});
 
 export default ProductEdit;
