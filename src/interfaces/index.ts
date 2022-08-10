8ee6138aa06b4fc365a159cc8caeaad1aca500f9
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-08-10 11:35:05 +0100

    chore: use number on drink_id and removed unused interfaces

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,5 +1,5 @@
-import { DocumentData, Timestamp } from 'firebase/firestore';
-import React, { ChangeEvent } from 'react';
+import React from 'react';
+import { DocumentData } from 'firebase/firestore';
 import { UseFormRegister } from 'react-hook-form';
 import { CategoryType } from 'src/types';
 
@@ -11,42 +11,15 @@ export interface UploadFormState {
 }
 
 export interface DrinksCategory {
-  drink_id: string;
+  drink_id: number;
   drinkCategory: CategoryType;
 }
-
-export interface InputFieldProps {
-  name: string;
-  handleChange: (
-    e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>
-  ) => void;
-  value: string | number;
-  placeHolder: string;
-  label: string;
-  isRequired: boolean;
-  type?: React.HTMLInputTypeAttribute | undefined;
-}
-
-export interface SelectFieldProps extends InputFieldProps {}
 
 export interface FileInputProps {
   file: File | null;
   progress: number;
   setProgress: React.Dispatch<React.SetStateAction<number>>;
   setFile: React.Dispatch<React.SetStateAction<File | null>>;
-}
-
-export interface ProductsProps {
-  category: CategoryType;
-  createdAt: Timestamp;
-  description: string;
-  drinkName: string;
-  price: number;
-  url: string;
-}
-
-export interface QueryClientWrapperProps {
-  children: React.ReactNode;
 }
 
 export interface Admin {
