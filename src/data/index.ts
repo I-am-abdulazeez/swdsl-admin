57cc321d9df075a3834311d3c38752dde64364c8
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-08-10 11:34:05 +0100

    chore: reoved naniod to use number index

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,66 +1,65 @@
 import { DrinksCategory } from '@interfaces/index';
-import { nanoid } from 'nanoid';
 
 export const drinkCategoriesArray: DrinksCategory[] = [
   {
     drinkCategory: 'Non alcoholic',
-    drink_id: nanoid(4),
+    drink_id: 1,
   },
   {
     drinkCategory: 'Whiskey',
-    drink_id: nanoid(4),
+    drink_id: 2,
   },
   {
     drinkCategory: 'Cognac',
-    drink_id: nanoid(4),
+    drink_id: 3,
   },
   {
     drinkCategory: 'Red wine',
-    drink_id: nanoid(4),
+    drink_id: 4,
   },
   {
     drinkCategory: 'Champagne',
-    drink_id: nanoid(4),
+    drink_id: 5,
   },
   {
     drinkCategory: 'Rum',
-    drink_id: nanoid(4),
+    drink_id: 6,
   },
   {
     drinkCategory: 'Irish cream',
-    drink_id: nanoid(4),
+    drink_id: 7,
   },
   {
     drinkCategory: 'White wine',
-    drink_id: nanoid(4),
+    drink_id: 8,
   },
   {
     drinkCategory: 'Gin',
-    drink_id: nanoid(4),
+    drink_id: 9,
   },
   {
     drinkCategory: 'Sparkling wine',
-    drink_id: nanoid(4),
+    drink_id: 10,
   },
   {
     drinkCategory: 'Brandy',
-    drink_id: nanoid(4),
+    drink_id: 11,
   },
   {
     drinkCategory: 'Juice',
-    drink_id: nanoid(4),
+    drink_id: 12,
   },
   {
     drinkCategory: 'Tequila',
-    drink_id: nanoid(4),
+    drink_id: 13,
   },
   {
     drinkCategory: 'Vodka',
-    drink_id: nanoid(4),
+    drink_id: 14,
   },
   {
     drinkCategory: 'Others',
-    drink_id: nanoid(4),
+    drink_id: 15,
   },
 ];
 
