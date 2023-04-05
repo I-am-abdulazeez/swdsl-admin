5e1b4395de75b8a202eb3dfa1a8071f7911afdea
AbdulAzeez <abdulazeezolamide50@gmail.com>
2023-04-05 19:48:04 +0100

    chore: remove Non alcoholic from drink category, since we have juice

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,65 +1,65 @@
 import { DrinksCategory } from '@interfaces/index';
 
 export const drinkCategoriesArray: DrinksCategory[] = [
+  // {
+  //   drinkCategory: 'Non alcoholic',
+  //   drink_id: 1,
+  // },
   {
-    drinkCategory: 'Non alcoholic',
+    drinkCategory: 'Whiskey',
     drink_id: 1,
   },
   {
-    drinkCategory: 'Whiskey',
+    drinkCategory: 'Cognac',
     drink_id: 2,
   },
   {
-    drinkCategory: 'Cognac',
+    drinkCategory: 'Red wine',
     drink_id: 3,
   },
   {
-    drinkCategory: 'Red wine',
+    drinkCategory: 'Champagne',
     drink_id: 4,
   },
   {
-    drinkCategory: 'Champagne',
+    drinkCategory: 'Rum',
     drink_id: 5,
   },
   {
-    drinkCategory: 'Rum',
+    drinkCategory: 'Irish cream',
     drink_id: 6,
   },
   {
-    drinkCategory: 'Irish cream',
+    drinkCategory: 'White wine',
     drink_id: 7,
   },
   {
-    drinkCategory: 'White wine',
+    drinkCategory: 'Gin',
     drink_id: 8,
   },
   {
-    drinkCategory: 'Gin',
+    drinkCategory: 'Sparkling wine',
     drink_id: 9,
   },
   {
-    drinkCategory: 'Sparkling wine',
+    drinkCategory: 'Brandy',
     drink_id: 10,
   },
   {
-    drinkCategory: 'Brandy',
+    drinkCategory: 'Juice',
     drink_id: 11,
   },
   {
-    drinkCategory: 'Juice',
+    drinkCategory: 'Tequila',
     drink_id: 12,
   },
   {
-    drinkCategory: 'Tequila',
+    drinkCategory: 'Vodka',
     drink_id: 13,
   },
   {
-    drinkCategory: 'Vodka',
-    drink_id: 14,
-  },
-  {
     drinkCategory: 'Others',
-    drink_id: 15,
+    drink_id: 14,
   },
 ];
 
