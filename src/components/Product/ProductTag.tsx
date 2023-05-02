31c1c1fffff2605bc6298a554e187f7bd68ed9ce
AbdulAzeez <abdulazeezolamide50@gmail.com>
2023-05-02 10:52:59 +0100

    chore: packSize

--- src/components/Product/ProductTag.tsx
+++ src/components/Product/ProductTag.tsx
@@ -8,8 +8,19 @@ const ProductTag: React.FC<ProductType> = ({ product }) => {
         <Tag
           fontWeight={'semibold'}
           fontSize={'12px'}
+          variant={'outline'}
           colorScheme={
-            product?.packSize === '12 Packs' ? 'primary' : 'secondary'
+            product?.packSize === '3 Packs'
+              ? 'primary'
+              : product?.packSize === '4 Packs'
+              ? 'green'
+              : product?.packSize === '6 Packs'
+              ? 'pink'
+              : product?.packSize === '10 Packs'
+              ? 'orange'
+              : product?.packSize === '12 Packs'
+              ? 'yellow'
+              : undefined
           }
         >
           {product?.packSize}
